5cc6248dd2a706c5ba004dfb2656e0262a22cecb
Abdo <abdessamadelfathi2018@gmail.com>
2023-03-24 14:08:13 +0000

    Admin Role and middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends Controller
     protected function authenticated()
     {
         if (Auth::user()->hasRole('admin')) {
-            return redirect('admin/dashboard')->with('status', 'Welcome to dashboard');
+            return redirect('admin/dashboard')->with('message', 'Welcome to dashboard');
         }
 
         return redirect('/home')->with('status', 'Logged in successfully');
